37cf980caf40737dfee5a6588bd20a35a46c6c95
Freddy Wordingham <freddy@digilab.co.uk>
2024-09-07 16:32:48 +0100

    Added camera speed controls

--- src/input/constants.rs
+++ src/input/constants.rs
@@ -9,3 +9,9 @@ pub const CAMERA_RIGHT: KeyCode = KeyCode::KeyD;
 pub const CAMERA_UP: KeyCode = KeyCode::KeyW;
 pub const CAMERA_ZOOM_IN: KeyCode = KeyCode::KeyE;
 pub const CAMERA_ZOOM_OUT: KeyCode = KeyCode::KeyQ;
+pub const CAMERA_SPEED_BOOST: KeyCode = KeyCode::ShiftLeft;
+
+pub const CAMERA_PAN_SPEED: f32 = 500.0;
+pub const CAMERA_ZOOM_SPEED: f32 = 0.01;
+pub const CAMERA_ZOOM_MAX: f32 = 0.1;
+pub const CAMERA_SPEED_BOOST_MULTIPLIER: f32 = 4.0;
--- src/input/systems/move_camera.rs
+++ src/input/systems/move_camera.rs
@@ -1,8 +1,6 @@
 use bevy::prelude::*;
 
-use super::super::constants::{
-    CAMERA_DOWN, CAMERA_LEFT, CAMERA_RIGHT, CAMERA_UP, CAMERA_ZOOM_IN, CAMERA_ZOOM_OUT,
-};
+use super::super::constants::*;
 
 pub fn move_camera(
     time: Res<Time>,
@@ -12,36 +10,41 @@ pub fn move_camera(
     for (mut transform, mut ortho) in query.iter_mut() {
         let mut direction = Vec3::ZERO;
 
+        let mut speed_multiplier = 1.0;
+        if keyboard_input.pressed(CAMERA_SPEED_BOOST) {
+            speed_multiplier = CAMERA_SPEED_BOOST_MULTIPLIER;
+        }
+
         if keyboard_input.pressed(CAMERA_LEFT) {
-            direction -= Vec3::new(1.0, 0.0, 0.0);
+            direction -= Vec3::new(CAMERA_PAN_SPEED, 0.0, 0.0);
         }
 
         if keyboard_input.pressed(CAMERA_RIGHT) {
-            direction += Vec3::new(1.0, 0.0, 0.0);
+            direction += Vec3::new(CAMERA_PAN_SPEED, 0.0, 0.0);
         }
 
         if keyboard_input.pressed(CAMERA_UP) {
-            direction += Vec3::new(0.0, 1.0, 0.0);
+            direction += Vec3::new(0.0, CAMERA_PAN_SPEED, 0.0);
         }
 
         if keyboard_input.pressed(CAMERA_DOWN) {
-            direction -= Vec3::new(0.0, 1.0, 0.0);
+            direction -= Vec3::new(0.0, CAMERA_PAN_SPEED, 0.0);
         }
 
         if keyboard_input.pressed(CAMERA_ZOOM_IN) {
-            ortho.scale -= 0.1;
+            ortho.scale -= CAMERA_ZOOM_SPEED * speed_multiplier;
         }
 
         if keyboard_input.pressed(CAMERA_ZOOM_OUT) {
-            ortho.scale += 0.1;
+            ortho.scale += CAMERA_ZOOM_SPEED * speed_multiplier;
         }
 
-        if ortho.scale < 0.5 {
-            ortho.scale = 0.5;
+        if ortho.scale < CAMERA_ZOOM_MAX {
+            ortho.scale = CAMERA_ZOOM_MAX;
         }
 
         let z = transform.translation.z;
-        transform.translation += time.delta_seconds() * direction * 500.0;
+        transform.translation += time.delta_seconds() * direction * speed_multiplier;
 
         // Important! We need to restore the Z values when moving the camera around.
         // Bevy has a specific camera setup and this can mess with how our layers are shown.
