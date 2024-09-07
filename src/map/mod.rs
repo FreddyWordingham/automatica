884d84c434539be4ea62018a7ad8a18f6552fe8c
Freddy Wordingham <freddy@digilab.co.uk>
2024-09-07 14:56:29 +0100

    Added cursor tile location systems

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -1,11 +1,63 @@
 use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 
+// RESOURCES
+
+#[derive(Resource)]
+pub struct CursorPos(Vec2);
+impl Default for CursorPos {
+    fn default() -> Self {
+        Self(Vec2::new(-1000.0, -1000.0))
+    }
+}
+
+// PLUGIN
+
 pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(TilemapPlugin).add_systems(Startup, setup);
+        app.add_plugins(TilemapPlugin)
+            .init_resource::<CursorPos>()
+            .add_systems(Startup, setup)
+            .add_systems(First, update_cursor_pos)
+            .add_systems(Update, report_cursor_position);
+    }
+}
+
+// SYSTEMS
+
+fn report_cursor_position(
+    cursor_pos: Res<CursorPos>,
+    tilemap_q: Query<(&TilemapSize, &TilemapGridSize, &TilemapType, &Transform)>,
+) {
+    for (map_size, grid_size, map_type, map_transform) in tilemap_q.iter() {
+        let cursor_pos: Vec2 = cursor_pos.0;
+        let cursor_in_map_pos: Vec2 = {
+            let cursor_pos = Vec4::from((cursor_pos, 0.0, 1.0));
+            let cursor_in_map_pos = map_transform.compute_matrix().inverse() * cursor_pos;
+            cursor_in_map_pos.xy()
+        };
+
+        if let Some(tile_pos) =
+            TilePos::from_world_pos(&cursor_in_map_pos, map_size, grid_size, map_type)
+        {
+            println!("Cursor in tile pos: {:?}", tile_pos);
+        }
+    }
+}
+
+pub fn update_cursor_pos(
+    camera_q: Query<(&GlobalTransform, &Camera)>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mut cursor_pos: ResMut<CursorPos>,
+) {
+    for cursor_moved in cursor_moved_events.read() {
+        for (cam_t, cam) in camera_q.iter() {
+            if let Some(pos) = cam.viewport_to_world_2d(cam_t, cursor_moved.position) {
+                *cursor_pos = CursorPos(pos);
+            }
+        }
     }
 }
 
